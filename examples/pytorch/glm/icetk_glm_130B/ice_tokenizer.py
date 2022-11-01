51e02c7f7887b041a6f000ad2634cfc8544a34d5
Aohan Zeng <Sengxian@live.com>
2022-11-01 12:53:25 +0800

    Update ice_tokenizer.py

--- examples/pytorch/glm/icetk_glm_130B/ice_tokenizer.py
+++ examples/pytorch/glm/icetk_glm_130B/ice_tokenizer.py
@@ -21,7 +21,8 @@ class _IceTokenizer(AbstractTokenizer):
         except ImportError:
             pass
         self.num_tokens = 150000
-        self.set_special_tokens(['MASK', 'gMASK', 'sMASK', 'eod', 'sop', 'eop', 'ENC', 'dBLOCK'] + ['<t>'] + [f'<blank_{i}>' for i in range(2, max_blank_len + 1)])
+        self.add_special_tokens(['[MASK]', '[gMASK]', '[sMASK]', 'eod', 'sop', 'eop', 'ENC', 'dBLOCK'] + ['<t>'] + [f'<blank_{i}>' for i in range(2, max_blank_len + 1)])
+
         self.sentence_end_decoder = {20007: '.', 20031: '？', 20035: '！', 20027: '；', 20012: ':', 83823: '。', 145670: '…'}
 
         self.special_tokens['eos'] = 20002
